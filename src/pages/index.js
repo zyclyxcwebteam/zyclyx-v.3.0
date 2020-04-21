feac55116b41243bc40049097019819ff19c00f6
sandeep-zyclyx <sandeep.reddy@zyclyx.com>
2020-04-21 18:48:32 +0530

    Added navbar and services dropdown in navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { Link } from 'gatsby'
+import Navbar from "../components/navbar"
+import ServiceDropdown from "../components/serviceDropdown"
 
 
 export default () => {
     return(
         <>
+        <Navbar />
+        <ServiceDropdown />
             <Link to ="/aboutus">aboutus</Link>
+            <h1>This is index</h1>
         </>
     )
 }
