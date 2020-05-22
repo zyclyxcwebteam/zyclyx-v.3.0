dddfddd3f4aa86856a9d287201cf5d4faf659c97
skanjarla <skanjarla@zyclyx.com>
2020-05-22 17:14:20 +0530

    merge changes

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import "./HeroBanner.css";
-import RPABannerImage from "../../images/rpa.svg";
 
-const HeroBanner = ({ titleOne, titleTwo }) => {
+const HeroBanner = ({ title, image }) => {
   return (
     <Container fluid className="hero-banner">
       <Row className="">
         <Col className="title-box">
-          <h2 className="banner-title banner-title-one">{titleOne}</h2>
-          <h2 className="banner-title banner-title-two">{titleTwo}</h2>
+          <h2 className="banner-title banner-title-one">{title}</h2>
+          {/* <h2 className="banner-title banner-title-two">{titleTwo}</h2> */}
         </Col>
         <Col className="img-box">
-          <img src={RPABannerImage} alt="robotic process automation" />
+          <img src={image} alt="robotic process automation" />
         </Col>
       </Row>
     </Container>
