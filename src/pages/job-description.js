aaf525e583876a224a0f88332d02986083ced35d
sandeep-zyclyx <sandeep.reddy@zyclyx.com>
2020-05-28 17:42:50 +0530

    added float label

--- src/pages/job-description.js
+++ src/pages/job-description.js
@@ -1,16 +1,63 @@
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/destructuring-assignment */
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Layout from "../components/Layout/Layout";
 import HeroBanner from "../components/HeroBanner/HeroBanner";
-import Button from "../components/Button/Button";
+// import Button from "../components/Button/Button";
 import "../css/job-description.css";
 
 class jobDescription extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      showReq: true,
+      showRes: false,
+      showForm: false,
+      btnState: true,
+    };
+    this.handleReqClick = this.handleReqClick.bind(this);
+    this.handleResClick = this.handleResClick.bind(this);
+    this.handleFormClick = this.handleFormClick.bind(this);
+  }
+
+  handleReqClick() {
+    console.log("Req");
+    this.setState({
+      showReq: true,
+      showRes: false,
+      showForm: false,
+      btnState: true,
+    });
+  }
+
+  handleResClick() {
+    console.log("Res");
+    this.setState({
+      showRes: true,
+      showReq: false,
+      showForm: false,
+      btnState: true,
+      // showForm: !this.State.showForm,
+    });
+    console.log(this.state.showRes);
+  }
+
+  handleFormClick() {
+    console.log("Form");
+    this.setState({
+      showForm: true,
+      showRes: false,
+      showReq: false,
+      btnState: false,
+    });
   }
 
   render() {
@@ -21,13 +68,27 @@ class jobDescription extends React.Component {
           <Row className="py-5">
             <Col md="2" className="p-0 d-flex flex-column align-items-center">
               <div className="">
-                <div className="req mb-5">Requirement</div>
-                <div className="res mb-5">Responsibilities</div>
-                <Button btntext="Apply Now" />
+                <div
+                  className="btn pl-0 req mb-5"
+                  onClick={this.handleReqClick}
+                >
+                  Requirement
+                </div>
+                <div
+                  className="btn pl-0 res mb-5"
+                  onClick={this.handleResClick}
+                >
+                  Responsibilities
+                </div>
+                {this.state.btnState ? (
+                  <div className="btn" onClick={this.handleFormClick}>
+                    Apply Now
+                  </div>
+                ) : null}
               </div>
             </Col>
             <Col md="10">
-              <div className="Requirement">
+              {this.state.showReq ? (
                 <div className="d-flex">
                   <span className="res-icon">
                     <FontAwesomeIcon icon={faArrowRight} className="" />
@@ -38,7 +99,25 @@ class jobDescription extends React.Component {
                     would be added advantage
                   </p>
                 </div>
-              </div>
+              ) : null}
+
+              {this.state.showRes ? (
+                <div className="d-flex">
+                  <span className="res-icon">
+                    <FontAwesomeIcon icon={faArrowRight} className="" />
+                  </span>
+                  <p className="ml-3 data">Res</p>
+                </div>
+              ) : null}
+
+              {this.state.showForm ? (
+                <div className="d-flex">
+                  <span className="res-icon">
+                    <FontAwesomeIcon icon={faArrowRight} className="" />
+                  </span>
+                  <p className="ml-3 data">Form</p>
+                </div>
+              ) : null}
             </Col>
           </Row>
         </Container>
