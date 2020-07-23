46bb19bb510c2966d1849965b8b9790468443df0
skanjarla <skanjarla@zyclyx.com>
2020-07-23 21:59:27 +0530

    Add SEO component

---
+++ src/components/SEO/seo.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const SEO = ({ description, lang, meta, title }) => {
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${title}`}
+      meta={[
+        {
+          name: `description`,
+          content: description,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: description,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: "",
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: description,
+        },
+      ].concat(meta)}
+    />
+  );
+};
+
+export default SEO;
