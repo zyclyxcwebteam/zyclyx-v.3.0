1f86160836fe997980096d19111ffdce9ca91640
skanjarla <skanjarla@zyclyx.com>
2020-09-09 16:24:01 +0530

    Fix typo in model title

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -71,7 +71,7 @@ const PageTemplate = props => {
       {title === "Staffing Solutions" && (
         <Container className="py-4">
           <h2 className="text-center py-3 overview-title">
-            Our Staffing Modals
+            Our Staffing Models
           </h2>
           <p className="text-center">
             We have designed different models to cater to your varied staffing
