f8f7fa4a89fad7615891e2a28d1fd890004dd160
skanjarla <skanjarla@zyclyx.com>
2020-10-13 14:19:24 +0530

    Add terms, privacy, sitemap

---
+++ src/pages/sitemap.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { Container } from "reactstrap";
+import Layout from "../components/Layout/Layout";
+import "../css/privacy.css";
+
+const SiteMap = () => {
+  return (
+    <Layout>
+      <Container fluid className="privacy-outer py-md-5 p-1 privacy-page">
+        <Container className="p-md-5 p-1 privacy-inner">
+          <h1>Site Map</h1>
+          <div className="py-2">
+            <a href="http://zyclyx.com/">Home page</a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/aboutus/">About us </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/contactus/">Contact us </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/careers/">Careers</a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/robotic-process-automation/">
+              Robotic Process Automation
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/artificial-intelligence/">
+              Artificial Intelligence
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/optical-character-recognition/">
+              Optical Character Recognition
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/blockchain/">Blockchain </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/internet-of-things/">
+              Internet of Things
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/bigdata/">Bigdata </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/network-security/">Network Security</a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/cyber-security/">Cyber Security</a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/application-data-security/">
+              Application & Data Security
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/governance-and-compliance/">
+              Governance & Compliance
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/endpoint-mobile-security/">
+              Endpoint & Mobile Security
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/software-development/">
+              Software Development
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/mobile-app-development/">
+              Mobile App Development
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/web-development/">Web Development</a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/digital-marketing/">Digital Marketing</a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/outsourcing-services/">
+              Outsourcing Services
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/staffing-solutions/">
+              Staffing Solutions
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/datacenter-services/">
+              Datacenter Services
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/system-infrastructure/">
+              System Infrastructure
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/end-user-computing/">
+              End User Computing
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/data-visualization/">
+              Data Visualization
+            </a>
+          </div>
+
+          <div className="py-2">
+            <a href="http://zyclyx.com/predictive-analytics/">
+              Predictive Analytics
+            </a>
+          </div>
+        </Container>
+      </Container>
+    </Layout>
+  );
+};
+
+export default SiteMap;
