98fb4505456045755d65e6a73160bee1c361e81c
skanjarla <skanjarla@zyclyx.com>
2020-10-16 10:56:15 +0530

    Fix responsive layout

--- src/components/Homepage/Industries/Industries.js
+++ src/components/Homepage/Industries/Industries.js
@@ -38,8 +38,14 @@ const IndContent = [
 
 const IndustryCard = ({ title, cardImg, updateContent }) => {
   return (
-    <div className={`i-card ${cardImg}`} onMouseEnter={updateContent}>
-      <h3 className="text-center card-title d-md-block d-none">{title}</h3>
+    <div
+      className={`d-flex align-items-center align-items-md-end i-card ${cardImg}`}
+      onMouseEnter={updateContent}
+    >
+      <h3 className="text-center card-title mb-0">
+        <span className="d-none d-md-block">{title}</span>
+        <span className="d-block d-md-none">{title[0]}</span>
+      </h3>
     </div>
   );
 };
