1407d48ffe084042beeeef1e2eaf38850e10b5ca
skanjarla <skanjarla@zyclyx.com>
2020-10-19 18:32:09 +0530

    Fix Layout horizontal scroll

--- src/components/Homepage/HomeBannerVideo/HomeBannerVideo.js
+++ src/components/Homepage/HomeBannerVideo/HomeBannerVideo.js
@@ -18,45 +18,43 @@ const HomeBannerVideo = () => {
     }
   });
   return (
-    <section className="home-banner">
-      <div className="row home-inner h-100 w-100">
-        <div className="col-12 order-1 order-md-0 order-sm-1 home-titles d-flex justify-content-center align-items-center">
-          <div className="hero-carousel">
-            <div className={`title-container ${index === 0 && "t-active"}`}>
-              <h1 className="banner-title light-text text-center">
-                Rooted in Knowledge
-                <br />
-                Built on Trust
-              </h1>
-            </div>
+    <section className="home-banner d-flex justify-content-center align-items-center">
+      <div className="home-titles">
+        <div className="hero-carousel">
+          <div className={`title-container ${index === 0 && "t-active"}`}>
+            <h1 className="banner-title light-text text-center">
+              Rooted in Knowledge
+              <br />
+              Built on Trust
+            </h1>
+          </div>
 
-            <div className={`title-container ${index === 1 && "t-active"}`}>
-              <h1 className="banner-title light-text text-center">
-                Taking Technology Forward
-                <br />
-                with Possibilities
-              </h1>
-            </div>
+          <div className={`title-container ${index === 1 && "t-active"}`}>
+            <h1 className="banner-title light-text text-center">
+              Taking Technology Forward
+              <br />
+              with Possibilities
+            </h1>
+          </div>
 
-            <div className={`title-container ${index === 2 && "t-active"}`}>
-              <h1 className="banner-title light-text text-center">
-                Save Time and Money with
-                <br />
-                Business Automation
-              </h1>
-            </div>
+          <div className={`title-container ${index === 2 && "t-active"}`}>
+            <h1 className="banner-title light-text text-center">
+              Save Time and Money with
+              <br />
+              Business Automation
+            </h1>
           </div>
         </div>
+      </div>
 
-        <div className="home-video">
-          <video
-            src="https://res.cloudinary.com/zyclyxweb/video/upload/v1603096683/zyclyx_bg_green.mp4"
-            id="bannerVideo"
-            autoPlay
-            muted
-            loop
-          />
-        </div>
+      <div className="home-video">
+        <video
+          src="https://res.cloudinary.com/zyclyxweb/video/upload/v1603096683/zyclyx_bg_green.mp4"
+          id="bannerVideo"
+          autoPlay
+          muted
+          loop
+        />
       </div>
     </section>
   );
